9715f53f7bef9bdede3f52839722d2b127361b55
billkuku <beierhao@hotmail.com>
2024-07-28 22:25:07 +0200

    upd

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // 使用环境变量 PORT，默认使用 3000
+
 const cors = require("cors");
 
 app.use(
@@ -10,11 +11,13 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-//
+
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
