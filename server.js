4611270e720e5893c32303f75b7b18071406ad97
billkuku <beierhao@hotmail.com>
2024-07-28 22:15:25 +0200

    update

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors");
 
+app.use(
+  cors({
+    origin: "*", // 允许所有来源的请求。如果你有特定的前端地址，可以指定，如 'http://localhost:5173'
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
